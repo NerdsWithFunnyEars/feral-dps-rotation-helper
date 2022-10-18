a05df7c2e5ccea7e742720f616ef1052fcb2c247
Insax <admin@1ns.at>
2022-10-18 21:07:48 +0200

    Change function name to match lua style

--- FeralByNerdDruids/Language.lua
+++ FeralByNerdDruids/Language.lua
@@ -7,7 +7,7 @@
 --- Returns the name of a given spell id
 --- @param spellID number
 --- @return string
-function getSpellName(spellID)
+function GetSpellName(spellID)
     local name = GetSpellInfo(spellID);
     return name;
 end
@@ -21,65 +21,65 @@ L = {
     --
 
     -- The thing we need but don't want to use
-    ["Mangle (Cat)"] = getSpellName(48566),
+    ["Mangle (Cat)"] = GetSpellName(48566),
     -- Bad bleed
-    ["Rake"] = getSpellName(48574),
+    ["Rake"] = GetSpellName(48574),
     -- Good bleed
-    ["Rip"] = getSpellName(49800),
+    ["Rip"] = GetSpellName(49800),
     -- Damage #1
-    ["Shred"] = getSpellName(48572),
+    ["Shred"] = GetSpellName(48572),
     -- Big bite
-    ["Ferocious Bite"] = getSpellName(48577),
+    ["Ferocious Bite"] = GetSpellName(48577),
     --
     --- Damage Abilities Bear
     --
 
     -- Filler
-    ["Mangle (Bear)"] = getSpellName(48564),
+    ["Mangle (Bear)"] = GetSpellName(48564),
     -- Okayish bleed
-    ["Lacerate"] = getSpellName(48568),
+    ["Lacerate"] = GetSpellName(48568),
     -- Ragedump
-    ["Maul"] = getSpellName(48480),
+    ["Maul"] = GetSpellName(48480),
 
     --
     --- Buffs
     --
 
     -- Aaah yes, 30% more dmg
-    ["Savage Roar"] = getSpellName(52610),
+    ["Savage Roar"] = GetSpellName(52610),
     -- Aaah yes, free shred
-    ["Clearcasting"] = getSpellName(16870),
+    ["Clearcasting"] = GetSpellName(16870),
 
     --
     --- Cooldowns
     --
 
     -- Aaah yes, energy
-    ["Tiger's Fury"] = getSpellName(50213),
+    ["Tiger's Fury"] = GetSpellName(50213),
     -- Time to kill smth
-    ["Berserk"] = getSpellName(50334),
+    ["Berserk"] = GetSpellName(50334),
     -- I'm in danger
-    ["Barkskin"] = getSpellName(22812),
+    ["Barkskin"] = GetSpellName(22812),
     -- Enemy is in danger
-    ["Enrage"] = getSpellName(5229),
+    ["Enrage"] = GetSpellName(5229),
 
     --
     --- External/Long Term Debuffs
     --
 
     -- Yay, warrior did a crit
-    ["Trauma"] = getSpellName(46855),
+    ["Trauma"] = GetSpellName(46855),
     -- Yay, there is a boomie in the group
-    ["Faerie Fire"] = getSpellName(770),
+    ["Faerie Fire"] = GetSpellName(770),
     -- Nay, no boomie here
-    ["Faerie Fire (Feral)"] = getSpellName(16857),
+    ["Faerie Fire (Feral)"] = GetSpellName(16857),
 
     --
     --- Shapeshifts
     --
 
     -- Swift kitten
-    ["Cat Form"] = getSpellName(768),
+    ["Cat Form"] = GetSpellName(768),
     -- Stronk bear
-    ["Dire Bear Form"] = getSpellName(9634)
+    ["Dire Bear Form"] = GetSpellName(9634)
 }
